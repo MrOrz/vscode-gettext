108ad6044d303fb78bd0fcbfe782fe4b0b04d267
Hiroki Tokunaga <tokusan441@gmail.com>
2024-02-14 09:21:44 +0900

    Logging

--- src/status.ts
+++ src/status.ts
@@ -43,6 +43,10 @@ async function runMsgfmtStatistics(): Promise<string | null> {
 
   return new Promise((resolve, reject) => {
     exec(command, (error, stdout, stderr) => {
+      console.log("error: ", error);
+      console.log("stdout: ", stdout);
+      console.log("stderr: ", stderr);
+
       // Correct. The command will print the statistics to stderr.
       resolve(stderr);
     });
